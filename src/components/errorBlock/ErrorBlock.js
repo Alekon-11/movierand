186c6b90b6f70248d356e1d2bd351e16cec3dfc8
Alex <konyaxin_a@mail.ru>
2022-09-01 17:37:19 +0300

    Added errorBlock

---
+++ src/components/errorBlock/ErrorBlock.js
@@ -0,0 +1,9 @@
+import './errorBlock.scss';
+
+const ErrorBlock = () => {
+    return(
+        <div className="error">Упс! Не удалось получить фильм! Попробуйте снова</div>
+    );
+}
+
+export default ErrorBlock;
--- src/components/movieGenerator/MovieGenerator.js
+++ src/components/movieGenerator/MovieGenerator.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 import MovieDBService from '../../services/MovieDBService';
 import Spinner from '../spinner/Spinner';
+import ErrorBlock from '../errorBlock/ErrorBlock';
 
 import './movieGenerator.scss';
 
@@ -13,8 +14,8 @@ const MovieGenerator = ({setMovieId}) => {
     const [error, setError] = useState(false);
 
     const loading = spinner ? <Spinner /> : null;
-    // const content = !spinner && !error ? <View movieInfo={movieInfo} /> : null;
     const content = !(spinner || error) ? <View movieInfo={movieInfo} /> : null;
+    const errorMessage = error ? <ErrorBlock /> : null;
 
     useEffect(() => {
         onMovieLoading();
@@ -46,6 +47,7 @@ const MovieGenerator = ({setMovieId}) => {
             <div className="movie-generator__info">
                 {loading}
                 {content}
+                {errorMessage}
             </div>
             <button onClick={onMovieLoading} className="btn">сгенерировать</button>
         </div>
